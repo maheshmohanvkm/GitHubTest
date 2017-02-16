2aa8d03af1a28306ecfff5bedadf60ababb070e5
Mahesh <Mahesh@10.0.0.7>
2017-02-16 17:53:17 +0530

    Git Test Demo

---
+++ GitTest/src/test/java/demo/PageTest.java
@@ -0,0 +1,42 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class PageTest 
+{
+	private WebDriver driver;
+	
+	@Test
+	public void checkTitle()
+	{
+		driver.get("http://www.guru99.com");
+		String actualTitle = "Meet Guru99";
+		String pageTitle = driver.getTitle();
+		System.out.println("Page Title = "+pageTitle);
+		if(pageTitle.contains(actualTitle))
+		{
+			System.out.println("Test Passed");
+		}
+		else
+		{
+			System.out.println("Test Failed");
+		}
+	}	
+	
+	@BeforeTest
+	public void beforeTest()
+	{	
+		    driver = new FirefoxDriver(); 
+		    driver.manage().window().maximize();
+	}		
+	
+	@AfterTest
+	public void afterTest()
+	{
+		driver.quit();			
+	}		
+}
